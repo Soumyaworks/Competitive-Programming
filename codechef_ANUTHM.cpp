72d751a1fa740057fa87fae8f608c8ff9c82ed53
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2020-04-20 10:00:48 +0530

    Create codechef_ANUTHM.cpp

---
+++ codechef_ANUTHM.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	double t,i,j,k,p,c,n,m;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n>>m;
+	    k=(n+m-1)*1.0;
+	    cout<<fixed<<setprecision(6)<<k<<"\n";
+	}
+	return 0;
+}
