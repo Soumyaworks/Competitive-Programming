d42aee5b0dbc3b4a0fa59068e4949c5d7a88f5a7
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2020-06-27 03:02:11 +0530

    Create codeforces_FashionabLee.cpp

---
+++ codeforces_FashionabLee.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+using namespace std;
+using namespace __gnu_pbds;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //cout<<"Hello World"<<"\n";
+    ll t,n,i,j,k,c,p;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        if(n%4==0)
+            cout<<"YES"<<"\n";
+        else
+            cout<<"NO"<<"\n";
+    }
+    return 0;
+}
