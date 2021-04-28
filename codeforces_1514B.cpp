39105994f01b9e2b89870281abab2c19f6260fe9
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2021-04-28 14:39:10 +0530

    Add files via upload

---
+++ codeforces_1514B.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+ll factorial(ll n)
+{
+    //const unsigned int M = 1000000007;
+ 
+    ll f = 1;
+    for (int i = 1; i <= n; i++)
+        f = (f*i) % mod;  // Now f never can
+                        // exceed 10^9+7
+    return f;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif 
+    */
+    //cout<<"Hello World"<<"\n";
+    ll n,k,t;
+    cin>>t;
+    while(t--){
+    cin>>n>>k;
+    ll j=(ll)pow(2,k)-1;
+    ll ans=1;
+    ll dig=log2(j)+1;
+    for(ll i=0;i<dig;i++){
+        ans=(ans*n)%mod;
+    }
+    cout<<ans%mod<<"\n";
+    }
+    return 0;
+}
