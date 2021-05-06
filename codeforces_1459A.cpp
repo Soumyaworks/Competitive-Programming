2f248a5bc44d13ccaf5702280b30a7df6816023d
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2021-05-07 00:28:40 +0530

    Add files via upload

---
+++ codeforces_1459A.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif 
+    */
+    //cout<<"Hello World"<<"\n";
+    int t,k,i,j,c,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        string a,b;
+        cin>>a>>b;
+        int r=0,bl=0;
+        for(i=0;i<n;i++){
+            if(a[i]>b[i])
+            r++;
+            else if(a[i]<b[i])
+            bl++;
+            else
+            continue;
+        }
+        if(r==bl)
+        cout<<"EQUAL"<<"\n";
+        else if(r>bl)
+        cout<<"RED"<<"\n";
+        else
+        cout<<"BLUE"<<"\n";
+    }
+    return 0;
+}
