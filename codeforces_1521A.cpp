c6298704652539b1e6277246df41c5bd178e9b21
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2021-05-08 01:46:35 +0530

    Add files via upload

---
+++ codeforces_1521A.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif 
+    */
+    //cout<<"Hello World"<<"\n";
+    ll t,n,i,j,k,a,b;
+    cin>>t;
+    while(t--){
+        cin>>a>>b;
+        ll x,y,z;
+        ll p=a*b;
+        x=1;
+        if(b==1){
+            cout<<"NO"<<"\n";
+        }
+        else{
+            /*
+            y=p-a;
+            k=2;
+            while(y==a||y%b==0){
+                p=p*k;
+                k++;
+                y=p-a;
+            }
+            ll v=(ll)pow(10,18);
+            if(y<v && p<v){
+            cout<<"YES"<<"\n";
+            cout<<a<<" "<<y<<" "<<p<<"\n";
+            }
+            else
+            cout<<"NO"<<"\n";
+            */
+            cout<<"YES"<<"\n";
+            cout<<a<<" "<<p<<" "<<p+a<<"\n";
+        }
+        
+    }
+    return 0;
+}
