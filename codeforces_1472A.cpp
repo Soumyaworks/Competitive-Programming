c5224432d5e283df7366c5963a3c71bf3a207a7e
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2021-05-12 02:25:44 +0530

    Add files via upload

---
+++ codeforces_1472A.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+void count(ll n,ll &p){
+    while(n%2==0){
+        n=n/2;
+        p=p*2;
+    }
+    //return p;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif 
+    */
+    //cout<<"Hello World"<<"\n";
+    ll t,i,j,k,c,p;
+    ll w,h,n;
+    cin>>t;
+    while(t--){
+        cin>>w>>h>>n;
+        p=1;
+        count(w,p);
+        count(h,p);
+        if(p>=n)
+        cout<<"YES"<<"\n";
+        else
+        cout<<"NO"<<"\n";
+    }
+    return 0;
+}
