8b7d88b32ebe4dba30311b9d6dfd5c9dfbddf795
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2021-06-03 03:10:14 +0530

    Add files via upload

---
+++ codeforces_1030B.cpp
@@ -0,0 +1,74 @@
+//Author : Soumya Banerjee
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+bool inside(int x,int y,int n,int d){
+    if((x+y)<d)
+    return false;
+    if((y-x)>d)
+    return false;
+    if((x+y)>(2*n-d))
+    return false;
+    if((x-y)>d)
+    return false;
+    return true;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif 
+    */
+    //cout<<"Hello World"<<"\n";
+    int n,d,m,i,k,j,c,p;
+    cin>>n>>d;
+    cin>>m;
+    while(m--){
+        int x,y;
+        cin>>x>>y;
+        if(inside(x,y,n,d)==true)
+        cout<<"YES"<<"\n";
+        else
+        cout<<"NO"<<"\n";
+    }
+
+    return 0;
+}
