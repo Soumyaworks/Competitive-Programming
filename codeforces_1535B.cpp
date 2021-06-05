7e9c2c244c139c3bc551747177a6c08eb2387db9
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2021-06-05 18:22:45 +0530

    Add files via upload

---
+++ codeforces_1535B.cpp
@@ -0,0 +1,79 @@
+//Author : Soumya Banerjee
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll int
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif 
+    */
+    //cout<<"Hello World"<<"\n";
+    ll t,n,i,j,k,c,p;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        vector<ll>a(n);
+        vector<ll>o,e;
+        for(i=0;i<n;i++){
+            cin>>a[i];
+            if(a[i]%2!=0)
+            o.pb(a[i]);
+            else
+            e.pb(a[i]);
+        }
+        c=0;
+        c+=(o.size()*e.size());;
+        c+=(e.size()*(e.size()-1))/2;
+        if(o.size()){
+        for(i=0;i<o.size()-1;i++){
+            for(j=i+1;j<o.size();j++){
+                if(__gcd(o[i],2*o[j])>1){
+                    c++;
+                }
+            }
+        }
+        }
+        cout<<c<<"\n";
+    }
+    return 0;
+}
