b5459c2e2c95916f95327e914ebcd4c4a665d472
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2021-07-07 19:11:48 +0530

    Add files via upload

---
+++ codeforces_1542B.cpp
@@ -0,0 +1,81 @@
+//Author : Soumya Banerjee
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif 
+    */
+    //cout<<"Hello World"<<"\n";
+    ll t,a,n,b;
+    cin>>t;
+    while(t--){
+        cin>>n>>a>>b;
+        if(b==1){
+            cout<<"Yes"<<"\n";
+        }
+        else if(a==1){
+            if((n)%b==1)
+            cout<<"Yes"<<"\n";
+            else
+            cout<<"No"<<"\n";
+        }
+        else{
+            ll k=0;ll p=1;
+            while(p<=n){
+                if((n-p)%b==0){
+                    k=1;
+                    break;
+                }
+                p=a*p;
+
+            }
+            if(k==1)
+            cout<<"Yes"<<"\n";
+            else
+            cout<<"No"<<"\n";
+        }
+    }
+    return 0;
+}
