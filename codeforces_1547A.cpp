7e6c1a64b03e1857aa92b99a488b70c006062309
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2021-07-12 14:43:36 +0530

    Add files via upload

---
+++ codeforces_1547A.cpp
@@ -0,0 +1,66 @@
+//Author : Soumya Banerjee
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif 
+    */
+    //cout<<"Hello World"<<"\n";
+    int xa,xb,xf,ya,yb,yf,t;
+    cin>>t;
+    while(t--){
+        char ch;
+        //cin>>ch;
+        cin>>xa>>ya;
+        cin>>xb>>yb;
+        cin>>xf>>yf;
+        if((xa==xb && xb==xf && (yf>ya && yf<yb || yf>yb && yf<ya)) || (ya==yb && yb==yf && (xf>xa && xf<xb || xf>xb && xf<xa))){
+            cout<<abs(xa-xb)+abs(ya-yb)+2<<"\n";
+        }
+        else
+        cout<<abs(xa-xb)+abs(ya-yb)<<"\n";
+    }
+    return 0;
+}
