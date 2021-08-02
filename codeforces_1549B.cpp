8d8854b171bdf2f4d6445e85c0aed9043d147858
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2021-08-02 09:44:37 +0530

    Add files via upload

---
+++ codeforces_1549B.cpp
@@ -0,0 +1,86 @@
+//Author : Soumya Banerjee
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif 
+    */
+    //cout<<"Hello World"<<"\n";
+    ll t,n,i,j,k,c,p;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        string s1,s2;
+        cin>>s1>>s2;
+        c=0;
+        for(i=0;i<n;i++){
+            if(s1[i]=='0' && s2[i]=='1')
+            c++;
+            else if(s1[i]=='1' && s2[i]=='1'){
+                
+                if(i>0 && s1[i-1]=='1'){
+                    s1[i-1]='2';
+                    c++;
+                }
+                else if(i<n-1 && s1[i+1]=='1'){
+                    s1[i+1]='2';
+                    c++;
+                }
+            }
+            else if(s2[i]=='1' && s1[i]=='2'){
+                if(i>0 && s1[i-1]=='1'){
+                    s1[i-1]='2';
+                    c++;
+                }
+                else if(i<n-1 && s1[i+1]=='1'){
+                    s1[i+1]='2';
+                    c++;
+                }
+            }
+        }
+        cout<<c<<"\n";
+    }
+    return 0;
+}
