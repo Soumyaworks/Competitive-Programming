e09a16d4284ac51b390013d274084fd3d22e3078
SOUMYA BANERJEE <54377824+Soumyaworks@users.noreply.github.com>
2022-03-28 22:20:14 +0530

    Create codechef_GROUPASSGN.cpp

---
+++ codechef_GROUPASSGN.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main(){
+    ll t,i,k,j,n,x;
+    cin>>t;
+    while(t--){
+        cin>>n>>x;
+        cout<<(2*n-x+1)<<"\n";
+    }
+    return 0;
+}
